f0b8e275795ad68949844d8486f832b9e2a99f1d
lishu <lilili925@icloud.com>
2022-01-08 18:29:30 +0800

    deploy

---
+++ precache-manifest.b7aa3b9ca3a511bc20f28d2d2106b7b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0341ae2d71a1cfb0c3f0",
+    "url": "/moneyWebsite/css/app.3d962c2b.css"
+  },
+  {
+    "revision": "f14185be7cf629c0332c3c7268486456",
+    "url": "/moneyWebsite/index.html"
+  },
+  {
+    "revision": "0341ae2d71a1cfb0c3f0",
+    "url": "/moneyWebsite/js/app.8ff8ad76.js"
+  },
+  {
+    "revision": "c8227d483ccb89111456",
+    "url": "/moneyWebsite/js/chunk-vendors.8a055649.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/moneyWebsite/manifest.json"
+  },
+  {
+    "revision": "10815eaa610d1770bc78ccf8f13eda06",
+    "url": "/moneyWebsite/qr_code.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneyWebsite/robots.txt"
+  }
+]);
