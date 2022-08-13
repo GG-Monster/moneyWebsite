41f8f41a7b3c02ca8c06f3d08f7147f17b83106a
GG-Monster <lilili925@icloud.com>
2022-08-13 23:09:26 +0800

    deploy

---
+++ precache-manifest.4b632210b2fec10daa00c2eb00050b7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3c4b54b68e869ce1b85",
+    "url": "/moneyWebsite/css/app.503a87b5.css"
+  },
+  {
+    "revision": "1d39a7ae5eca1d2a65238087a8fd1441",
+    "url": "/moneyWebsite/index.html"
+  },
+  {
+    "revision": "b3c4b54b68e869ce1b85",
+    "url": "/moneyWebsite/js/app.c5000512.js"
+  },
+  {
+    "revision": "c8227d483ccb89111456",
+    "url": "/moneyWebsite/js/chunk-vendors.8a055649.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/moneyWebsite/manifest.json"
+  },
+  {
+    "revision": "94e1bd7439ff00147a7a83d5f3e9380b",
+    "url": "/moneyWebsite/qr_code.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/moneyWebsite/robots.txt"
+  }
+]);
